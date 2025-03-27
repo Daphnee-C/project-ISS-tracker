0dd0b6380ce90d1c9823601d282755d0b65c6941
Daphnee-C <dadacc16@gmail.com>
2025-03-27 15:26:05 +0100

    init controller route

---
+++ controller/authController.js
@@ -0,0 +1,11 @@
+import User from "../models/User.js"
+
+
+export const registerUser = async(req, res) => {
+    console.log('hihi')
+}
+
+
+export const loginUser = async(req, res) => {
+    console.log('haha')
+}
